0d06b0c317d2e5292005a08a2e738f81e7bd2bfa
joowon <saven7788@gmail.com>
2017-09-09 19:25:52 +0900

    promise를 async로 보내기

--- migrations/migrate.js
+++ migrations/migrate.js
@@ -3,7 +3,9 @@ dotenv.load();
 
 const Meetup = require('../lib/models/meetup');
 const User = require('../lib/models/user');
+const Reservation = require('../lib/models/reservation');
 
 
-Meetup.sync().then(console.log).catch(console.error);
-User.sync().then(console.log).catch(console.error);
+Meetup.sync({force:true}).then(console.log).catch(console.error);
+User.sync({force:true}).then(console.log).catch(console.error);
+Reservation.sync({force:true}).then(console.log).catch(console.error);
